f8f364cb964553132b8b28012acf174043a95778
jhonjlinaresb <jhonjlinaresb@gmail.com>
2020-12-10 01:12:34 +0100

    Changes in the name of app

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,7 +59,7 @@
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Resetear Password') }}
+                                        {{ __('Resetear Password.') }}
                                     </a>
                                 @endif
                             </div>
